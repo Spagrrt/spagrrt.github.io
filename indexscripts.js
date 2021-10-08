55bf923afd8d21aa36cd6d5c10ac82b5fb334124
Spagrrt <80074923+Spagrrt@users.noreply.github.com>
2021-10-08 15:38:12 -0500

    Update indexscripts.js

--- indexscripts.js
+++ indexscripts.js
@@ -1,1 +1,4 @@
-
+document.getElementByClass("title").addEventListener("click", stoopid(this));
+function stoopid(element) {
+  element.innerHTML = "Hey this isn't what you are supposed to click stoopid";
+}
