dcdd99ea4e78a3169c410bba5af28cac48fc3bbf
Spagrrt <80074923+Spagrrt@users.noreply.github.com>
2021-11-18 15:25:55 -0600

    Delete indexscripts.js

--- indexscripts.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('title').onclick = function stoopid()
-function stoopid() {
-  document.getElementById('title').textContent = "Hey this isn't what you are supposed to click stoopid";
-}
